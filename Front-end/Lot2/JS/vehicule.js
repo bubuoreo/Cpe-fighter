08592473339b89362f937af894a94aabdc4fd134
Alexandre Burlot/usr/local/bin/python3 <burlotalexandre@yahoo.fr>
2022-06-01 11:11:43 +0200

    organisation

---
+++ Front-end/Lot2/JS/vehicule.js
@@ -0,0 +1,14 @@
+// document.getElementById("button")
+var variable = hello;
+
+function CreateVehicule() {
+    document.getElementById("buttonCreate").textContent="oui"
+}
+
+function ModifyVehicule() {
+    
+}
+
+function DeleteVehicule() {
+    
+}
