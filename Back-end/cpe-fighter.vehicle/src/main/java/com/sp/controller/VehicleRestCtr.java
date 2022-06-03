14c4823779ec9a00a184fe0758ded09f5016d570
QuentinMELERO <Quentin.melero@hotmail.fr>
2022-06-03 14:24:11 +0200

    a

--- Back-end/cpe-fighter.vehicle/src/main/java/com/sp/controller/VehicleRestCtr.java
+++ Back-end/cpe-fighter.vehicle/src/main/java/com/sp/controller/VehicleRestCtr.java
@@ -4,26 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.DTO.FireDTO;
 import com.DTO.VehicleDTO;
 import com.sp.service.VehicleService;
-import com.sp.tools.Comm;
+
 
 @RestController
 public class VehicleRestCtr {
 	
 	@Autowired
 	VehicleService vehicleService;
-	
-	@GetMapping("/manager")
-	public List<Object> manage() {
-		List<Object> returnList = vehicleService.manageFire();
-		// TODO : fin du feu --> retour à la caserne
-		return returnList;
-	}
 	
 	@GetMapping("/manager/vehicle")
 	public List<VehicleDTO> getVehicles() {
